b7d4640848596afa8935974c24193b372d55b780
Shaun Teo <xshaun97@gmail.com>
2020-03-16 23:57:25 +0800

    linked up rider page and buttons for login

--- frontend/src/components/views/Login/Login.jsx
+++ frontend/src/components/views/Login/Login.jsx
@@ -8,45 +8,47 @@ import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui
 import SelectView from './SelectView';
 import SignUp from './SignUp';
 
+import { Link } from "react-router-dom";
 
-// class Login extends Component {
-//   constructor() {
-//     super();
-//     this.state = {};
-//   }
 
-//   render() {
-//     return (
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {};
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-//     );
-//   }
-// }
+    handleClick() {
+        this.props.viewSelector();
+    }
 
-// export default Login;
+    render() {
+        return (
+            <>
+                <Segment raised>
 
-const Login = () => (
-    <>
-        <Segment raised>
+                    <Form size='large'>
+                        <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                        <Form.Input
+                            fluid
+                            icon='lock'
+                            iconPosition='left'
+                            placeholder='Password'
+                            type='password'
+                        />
+                        <Link to="/rider">
+                            <Button color='blue' fluid size='large'>
+                                Login
+                            </Button>
+                        </Link>
+                    </Form>
+                </Segment>
+                <Message>
+                    New to us? <a href='#' onClick={this.handleClick}>Sign Up</a>
+                </Message>
+            </>
+        );
+    }
+}
 
-            <Form size='large'>
-                <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-                <Form.Input
-                    fluid
-                    icon='lock'
-                    iconPosition='left'
-                    placeholder='Password'
-                    type='password'
-                />
-
-                <Button color='blue' fluid size='large'>
-                    Login
-          </Button>
-            </Form>
-        </Segment>
-        <Message>
-            New to us? <a href='#'>Sign Up</a>
-        </Message>
-    </>
-)
-
-export default Login
+export default Login;
--- frontend/src/components/views/Login/SignUp.jsx
+++ frontend/src/components/views/Login/SignUp.jsx
@@ -5,24 +5,41 @@ import React, { Component } from 'react';
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 
-const SignUp = () => (
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-  <Segment raised>
-    <Form size='large'>
-      <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-      <Form.Input
-        fluid
-        icon='lock'
-        iconPosition='left'
-        placeholder='Password'
-        type='password'
-      />
+  handleClick() {
+    this.props.viewSelector();
+  }
 
-      <Button color='blue' fluid size='large'>
-        Sign Up
+  render() {
+    return (
+      <>
+        <Segment raised>
+          <Form size='large'>
+            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+            <Form.Input
+              fluid
+              icon='lock'
+              iconPosition='left'
+              placeholder='Password'
+              type='password'
+            />
+
+            <Button color='blue' fluid size='large'>
+              Sign Up
           </Button>
-    </Form >
-  </Segment>
-)
+          </Form >
+        </Segment>
+        <Message>
+          Already with us? <a href='#' onClick={this.handleClick}>Login</a>
+        </Message>
+      </>
+    )
+  }
+}
 
 export default SignUp
