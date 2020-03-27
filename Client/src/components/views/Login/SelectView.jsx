a93e40f4cd38e0a2cd1c066ffb17dfb5fcc196bd
weng-kexin <weng.kexin98@gmail.com>
2020-03-27 23:07:47 +0800

    added popup message for icons when hovering mouse over

--- Client/src/components/views/Login/SelectView.jsx
+++ Client/src/components/views/Login/SelectView.jsx
@@ -1,45 +1,94 @@
 //Basic React Imports
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 //Semantic
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { Button, Header, Segment, Popup } from "semantic-ui-react";
 
 //Own css styling
-import './Login.css';
+import "./Login.css";
 
 class SelectView extends Component {
-    constructor(props) {
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    handleClick(e) {
-        this.props.viewSelector("viewLogin", e);
-    }
+  handleClick(e) {
+    this.props.viewSelector("viewLogin", e);
+  }
 
-    render() {
-        return (
-            <Segment raised>
-                <Header as='h3' color='blue' textAlign='center'>
-                    Please select your view
-                </Header>
+  render() {
+    return (
+      <Segment raised>
+        <Header as="h3" color="blue" textAlign="center">
+          Please select your view
+        </Header>
 
-                <span className="selectViewButton">
-                    <Button onClick={() => this.handleClick('customer')} size='massive' color='blue' circular icon='user'></Button>
-                </span>
-                <span className="selectViewButton">
-                    <Button onClick={() => this.handleClick('staff')} size='massive' color='blue' circular icon='settings'></Button>
-                </span>
-                <span className="selectViewButton">
-                    <Button onClick={() => this.handleClick('rider')} size='massive' color='blue' circular icon='motorcycle'></Button>
-                </span>
-                <span className="selectViewButton">
-                    <Button onClick={() => this.handleClick('manager')} size='massive' color='blue' circular icon='settings'></Button>
-                </span>
-            </Segment>
-        )
-    }
+        <span className="selectViewButton">
+          <Popup
+            trigger={
+              <Button
+                onClick={() => this.handleClick("customer")}
+                size="massive"
+                color="blue"
+                circular
+                icon="shopping cart"
+                hi
+              ></Button>
+            }
+            content="customer"
+            position="bottom center"
+          />
+        </span>
+        <span className="selectViewButton">
+          <Popup
+            trigger={
+              <Button
+                onClick={() => this.handleClick("staff")}
+                size="massive"
+                color="blue"
+                circular
+                icon="user"
+              ></Button>
+            }
+            content="staff"
+            position="bottom center"
+          />
+        </span>
+        <span className="selectViewButton">
+          <Popup
+            trigger={
+              <Button
+                onClick={() => this.handleClick("rider")}
+                size="massive"
+                color="blue"
+                circular
+                icon="motorcycle"
+              ></Button>
+            }
+            content="rider"
+            position="bottom center"
+          />
+        </span>
+        <span className="selectViewButton">
+          <Popup
+            trigger={
+              <Button
+                onClick={() => this.handleClick("manager")}
+                size="massive"
+                color="blue"
+                circular
+                icon="settings"
+              ></Button>
+            }
+            content="manager"
+            position="bottom center"
+          />
+        </span>
+      </Segment>
+    );
+  }
 }
 
-export default SelectView
+export default SelectView;
